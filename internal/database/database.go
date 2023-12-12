dc3544eff95d09433daf04bf116bc0e9e86041e6
Jorge Martinez <martinez.jo.luis.ca@gmail.com>
2023-12-12 16:34:41 -0500

    Modificación del dsn

--- internal/database/database.go
+++ internal/database/database.go
@@ -4,31 +4,36 @@ import (
 	"RedditAutoPost/config"
 	"database/sql"
 	"fmt"
+	"log"
 
-	_ "github.com/go-sql-driver/mysql"
+	"github.com/go-sql-driver/mysql"
 )
 
 var connection *sql.DB
 
 func Connect() (*sql.DB, error) {
-	isConfigEnable, err := config.InitializeConfig()
 
-	if !isConfigEnable {
-		fmt.Println("Error:", err)
+	dsn := mysql.Config{
+		AllowNativePasswords: true,
+		Net:                  "tcp",
+		DBName:               config.GetEnv("database.name"),
+		User:                 config.GetEnv("database.username"),
+		Passwd:               config.GetEnv("database.password"),
+		Addr:                 fmt.Sprintf("%s:%s", config.GetEnv("database.host"), config.GetEnv("database.port")),
 	}
 
-	hostname := config.GetEnv("database.host")
-	port := config.GetEnv("database.port")
-	username := config.GetEnv("database.username")
-	password := config.GetEnv("database.password")
-	name := config.GetEnv("database.name")
-
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", username, password, hostname, port, name)
-	connection, err := sql.Open("mysql", dsn)
+	connection, err := sql.Open("mysql", dsn.FormatDSN())
 
 	if err != nil {
 		return nil, err
 	}
+
+	pingErr := connection.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+
+	fmt.Println("Connected!")
 
 	return connection, nil
 }
