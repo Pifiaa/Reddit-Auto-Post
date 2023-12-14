b73fd99d777ecdc19efe63faa2038b91cdf26747
Jorge Martinez <martinez.jo.luis.ca@gmail.com>
2023-12-14 15:13:10 -0500

    Campos agreagdos y tamaños modificados

--- internal/database/migration/20231212124636_redditcredentials.sql
+++ internal/database/migration/20231212124636_redditcredentials.sql
@@ -1,10 +1,11 @@
 -- +goose Up
 CREATE TABLE RedditCredentials (
     id int NOT NULL,
-    username varchar,
-    password varchar,
-
-    PRIMARY KEY(id),
+    username varchar(255),
+    password varchar(255),
+    client_secret varchar(255),
+    client_id varchar(255),
+    PRIMARY KEY(id)
 )
 -- +goose StatementBegin
 -- +goose StatementEnd
