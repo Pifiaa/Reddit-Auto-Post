94022ef28b640f1bf2fa7a800f9cd3c0611244ca
Jorge Martinez <martinez.jo.luis.ca@gmail.com>
2023-12-15 12:44:28 -0500

    Modificación del nombre de la tabla, campos y tipo de datos

--- internal/models/credentials/credentials.go
+++ internal/models/credentials/credentials.go
@@ -2,11 +2,11 @@ package credentials
 
 import "gorm.io/gorm"
 
-type credentials struct {
+type Credentials struct {
 	gorm.Model
-	id            int    `json:"id" gorm:AUTO_INCREMENT;"primaryKey"`
-	usename       int    `json:"usename" gorm:"not null"`
-	password      int    `json:"password" gorm:"not null"`
-	client_secret string `json:"client_secret" gorm:"not null"`
-	client_id     string `json:"client_id" gorm:"not null"`
+	ID           uint   `json:"id" gorm:"AUTO_INCREMENT;"primaryKey"`
+	Username     string `json:"username" gorm:"not null"`
+	Password     string `json:"password" gorm:"not null"`
+	ClientSecret string `json:"client_secret" gorm:"not null"`
+	ClientID     string `json:"client_id" gorm:"not null"`
 }
