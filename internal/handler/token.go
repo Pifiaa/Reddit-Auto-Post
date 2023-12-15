e2e4f95ed3484afdb357d58e51f677e4f93ccce0
Jorge Martinez <martinez.jo.luis.ca@gmail.com>
2023-12-15 12:45:46 -0500

    Modificación en responsabilidades

--- internal/handler/token.go
+++ internal/handler/token.go
@@ -3,7 +3,6 @@ package handler
 import (
 	"RedditAutoPost/config"
 	"RedditAutoPost/internal/services"
-	"encoding/base64"
 	"fmt"
 
 	"github.com/gin-gonic/gin"
@@ -12,19 +11,5 @@ import (
 func GetAccessToken(c *gin.Context) {
 	url := fmt.Sprintf("%s/access_token", config.GetEnv("reddit.url"))
 
-	usename := "Piifia"
-	password := "123456789asd*"
-	client_secret := "1ptla1RhgWKykD9f53RaSyfVHj-FgA"
-	client_id := "9vDiuDZcj1CNfWBn7hhUbw"
-
-	authString := base64.StdEncoding.EncodeToString([]byte(client_id + ":" + client_secret))
-
-	data := fmt.Sprintf("grant_type=password&username=%s&password=%s", usename, password)
-
-	headers := map[string]string{
-		"Content-Type": "application/x-www-form-urlencoded",
-		"bar":          "Authorization " + authString,
-	}
-
-	services.Auth(url, headers, data, c)
+	services.Auth(url, c)
 }
