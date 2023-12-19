36d3989d012541fa0bb7026e084bf199ecf4917a
Jorge Martinez <martinez.jo.luis.ca@gmail.com>
2023-12-19 12:18:59 -0500

    returno agregados

--- internal/services/credentials.go
+++ internal/services/credentials.go
@@ -6,11 +6,12 @@ import (
 	"fmt"
 )
 
-func GetCredentials() credentials.Credentials {
+func GetCredentials() (credentials.Credentials, error) {
 	db, err := database.Connect()
 
 	if err != nil {
 		fmt.Errorf("Error al conectar la base de datos: %w", err)
+		return credentials.Credentials{}, err
 	}
 
 	var redditCredential []credentials.Credentials
@@ -18,8 +19,8 @@ func GetCredentials() credentials.Credentials {
 
 	if len(redditCredential) == 0 {
 		fmt.Errorf("No hay credenciales de acceso disponibles")
-
+		return credentials.Credentials{}, err
 	}
 
-	return redditCredential[0]
+	return redditCredential[0], nil
 }
