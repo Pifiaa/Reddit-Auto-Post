545ed2eaa74803c95b56d3f17a3adde006b68905
Jorge Martinez <martinez.jo.luis.ca@gmail.com>
2023-12-20 16:52:54 -0500

    Validación de endpoints y nuevo endpoint agregado

--- api/routes/routes.go
+++ api/routes/routes.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"RedditAutoPost/internal/handler"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,7 +12,7 @@ import (
 //
 // Parámetros:
 //   - route: El router de Gin al que se agregarán las rutas.
-func Routes(route *gin.Engine) {
+func Routes(route *gin.Engine) error {
 
 	// Crear un grupo de rutas para versionar la API.
 	redditGroup := route.Group("/api/v1")
@@ -21,5 +22,15 @@ func Routes(route *gin.Engine) {
 
 		// Definir la ruta para crear un nuevo post.
 		redditGroup.POST("/post", handler.CreatePost)
+
+		// Agrega credenciales de autenticación
+		redditGroup.POST("/create-credentials", handler.CreateCredentials)
 	}
+
+	// Manejar el caso de ruta no encontrada.
+	route.NoRoute(func(c *gin.Context) {
+		c.JSON(http.StatusNotFound, gin.H{"message": "Ruta no encontrada"})
+	})
+
+	return nil
 }
