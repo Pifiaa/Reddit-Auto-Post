d0f4f286f81b1c77945b6212e9f2a10296aeb08f
Jorge Martinez <martinez.jo.luis.ca@gmail.com>
2024-01-22 16:27:34 -0500

    Modificaión de mensaje y cambioi de tipo de conexión

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"RedditAutoPost/config"
 	"RedditAutoPost/internal/database"
+	"RedditAutoPost/internal/server"
 	"fmt"
 	"log"
 )
@@ -10,10 +11,9 @@ import (
 func main() {
 	config, err := config.InitConfig()
 	if err != nil {
-		log.Fatalln("Failed to load environment variables! \n", err.Error())
+		log.Fatalln("Error al cargar variables de entorno! \n", err.Error())
 	}
 
-	fmt.Println(config.Db)
 	connection, err := database.DatabaseConnect(config)
 	if err != nil {
 		fmt.Printf("Error al conectar la base de datos: %v\n", err)
@@ -21,5 +21,6 @@ func main() {
 	}
 	defer connection.Close()
 
-	// server.StartServer()
+	// server.StartServer(config)
+	server.StartServer(config)
 }
